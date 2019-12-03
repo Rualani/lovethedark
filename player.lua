e3f24b521ebe69e1b347e39304860bbc8e14e86f
Joshua Thatcher <eninakra@gmail.com>
2019-12-02 21:29:53 -0500

    Added data for player collisions. NEED ... data analysis

--- player.lua
+++ player.lua
@@ -2,9 +2,26 @@ player = {}
 player.x = resWidth/2
 player.y = resHeight/2
 player.speed = 200
+player.shadowed = false
+player.pure = true
+player.xtile = math.floor(player.x)
+player.ytile = math.floor(player.y)
 
 function playerUpdate(dt)
 
+   -- COLLISIONS AND STUFF LOL
+   player.xtile = math.floor(player.x)
+   player.ytile = math.floor(player.y)
+
+   -- Don't worry, You aren't shadowed, be pure and whole
+   
+
+
+   -- Something... is there isn't it. It hurts... doesn't it?
+
+
+
+   -- change to new position based on keyboard input
   if love.keyboard.isDown("w") then
     player.y = player.y - player.speed * dt
   end
@@ -17,5 +34,7 @@ function playerUpdate(dt)
   if love.keyboard.isDown("d") then
     player.x = player.x + player.speed * dt
   end
+
+
 
 end
